ee831b6d650f10a19950d3332c10117c701e738c
balazs.zsoldos <balazs.zsoldos@everit.biz>
2014-11-06 12:37:03 +0100

    Renaming project to Component RI and splitting API project

--- core/src/main/java/org/everit/osgi/ecm/component/internal/attribute/ReferenceHelper.java
+++ core/src/main/java/org/everit/osgi/ecm/component/internal/attribute/ReferenceHelper.java
@@ -1,25 +1,25 @@
 /**
- * This file is part of Everit - ECM Component.
+ * This file is part of Everit - ECM Component RI.
  *
- * Everit - ECM Component is free software: you can redistribute it and/or modify
+ * Everit - ECM Component RI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Everit - ECM Component is distributed in the hope that it will be useful,
+ * Everit - ECM Component RI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Everit - ECM Component.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Everit - ECM Component RI.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.everit.osgi.ecm.component.internal.attribute;
 
 import org.everit.osgi.capabilitycollector.AbstractCapabilityCollector;
 import org.everit.osgi.capabilitycollector.CapabilityConsumer;
 import org.everit.osgi.capabilitycollector.Suiting;
-import org.everit.osgi.ecm.component.context.ComponentContext;
+import org.everit.osgi.ecm.component.ComponentContext;
 import org.everit.osgi.ecm.component.internal.ReferenceEventHandler;
 import org.everit.osgi.ecm.metadata.ReferenceMetadata;
 
--- core/src/main/java/org/everit/osgi/ecm/component/internal/attribute/ServiceReferenceAttributeHelper.java
+++ core/src/main/java/org/everit/osgi/ecm/component/internal/attribute/ServiceReferenceAttributeHelper.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Everit - ECM Component.
+ * This file is part of Everit - ECM Component RI.
  *
- * Everit - ECM Component is free software: you can redistribute it and/or modify
+ * Everit - ECM Component RI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Everit - ECM Component is distributed in the hope that it will be useful,
+ * Everit - ECM Component RI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Everit - ECM Component.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Everit - ECM Component RI.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.everit.osgi.ecm.component.internal.attribute;
 
@@ -22,7 +22,7 @@ import org.everit.osgi.capabilitycollector.AbstractCapabilityCollector;
 import org.everit.osgi.capabilitycollector.RequirementDefinition;
 import org.everit.osgi.capabilitycollector.ServiceReferenceCollector;
 import org.everit.osgi.capabilitycollector.Suiting;
-import org.everit.osgi.ecm.component.context.ComponentContext;
+import org.everit.osgi.ecm.component.ComponentContext;
 import org.everit.osgi.ecm.component.internal.ReferenceEventHandler;
 import org.everit.osgi.ecm.metadata.ServiceReferenceMetadata;
 import org.osgi.framework.ServiceReference;
@@ -30,7 +30,7 @@ import org.osgi.framework.ServiceReference;
 public class ServiceReferenceAttributeHelper<S, COMPONENT> extends
         ReferenceHelper<ServiceReference<S>, COMPONENT> {
 
-    private Method bindMethod;
+    private Method setterMethod;
 
     private final ServiceReferenceMetadata serviceReferenceMetadata;
 
@@ -45,13 +45,15 @@ public class ServiceReferenceAttributeHelper<S, COMPONENT> extends
 
     @Override
     protected void bindInternal() {
-        if (bindMethod == null) {
+        if (setterMethod == null) {
             return;
         }
 
         Suiting<ServiceReference<S>>[] tmpSuitings = getSuitings();
         // TODO re-use references if necessary
         for (Suiting<ServiceReference<S>> suiting : tmpSuitings) {
+            ServiceReference<S> capability = suiting.getCapability();
+            RequirementDefinition<ServiceReference<S>> requirement = suiting.getRequirement();
 
         }
     }
