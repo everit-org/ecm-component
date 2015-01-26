caf0713869ca5bf143a366f783f674bdb62a8c3c
balazs.zsoldos <balazs.zsoldos@everit.biz>
2015-01-26 02:18:33 +0100

    Ongoing development

--- core/src/main/java/org/everit/osgi/ecm/component/ri/internal/ComponentRevisionImpl.java
+++ core/src/main/java/org/everit/osgi/ecm/component/ri/internal/ComponentRevisionImpl.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Everit - ECM Component RI.
+ *
+ * Everit - ECM Component RI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - ECM Component RI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - ECM Component RI.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.everit.osgi.ecm.component.ri.internal;
 
 import java.util.List;
